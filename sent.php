270da0bc8228a5097a0fbf818247aad3a0720ac4
blinkzzy <68962747+blinkzzy@users.noreply.github.com>
2023-03-28 01:06:54 -0700

    Add files via upload

---
+++ sent.php
@@ -0,0 +1,149 @@
+<!doctype html>
+                        <html>
+                            <head>
+                                <meta charset='utf-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <title>Snippet - GoSNippets</title>
+                                <link href='css/stly.css' rel='stylesheet'>
+                                <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
+                                <style>.succes {
+    background-color: #4BB543;
+}
+
+.succes-animation {
+    animation: succes-pulse 2s infinite;
+}
+
+.danger {
+    background-color: #9b2620;
+}
+
+.danger-animation {
+    animation: danger-pulse 2s infinite;
+}
+
+
+
+
+.custom-modal {
+    position: relative;
+    width: 350px;
+    min-height: 250px;
+    background-color: #584e4e;
+    border-radius: 30px;
+    margin: 40px 10px;
+}
+
+.custom-modal .content {
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    bottom: 0;
+}
+
+.custom-modal .content .type {
+    font-size: 18px;
+    color: #999;
+}
+
+.custom-modal .content .message-type {
+    font-size: 24px;
+    color: #000;
+}
+
+.custom-modal .border-bottom {
+    position: absolute;
+    width: 300px;
+    height: 20px;
+    border-radius: 0 0 30px 30px;
+    bottom: -20px;
+    margin: 0 25px;
+}
+
+.custom-modal .icon-top {
+    position: absolute;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    top: -30px;
+    margin: 0 125px;
+    font-size: 30px;
+    color: #fff;
+    line-height: 100px;
+    text-align: center;
+}
+
+@keyframes succes-pulse {
+    0% {
+        box-shadow: 0px 0px 30px 20px rgba(75, 181, 67, .2);
+    }
+
+    50% {
+        box-shadow: 0px 0px 30px 20px rgba(75, 181, 67, .4);
+    }
+
+    100% {
+        box-shadow: 0px 0px 30px 20px rgba(75, 181, 67, .2);
+    }
+}
+
+@keyframes danger-pulse {
+    0% {
+        box-shadow: 0px 0px 30px 20px rgba(202, 11, 0, .2);
+    }
+
+    50% {
+        box-shadow: 0px 0px 30px 20px rgba(202, 11, 0, .4);
+    }
+
+    100% {
+        box-shadow: 0px 0px 30px 20px rgba(202, 11, 0, .2);
+    }
+}
+
+
+.page-wrapper {
+    height: 90vh;
+    background-color: #322e2e;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 0;
+}
+
+body {
+    margin: 0;
+    font-family: 'Roboto';
+}
+
+@media only screen and (max-width: 800px) {
+    .page-wrapper {
+        flex-direction: column;
+    }
+}</style>
+                                <script type='text/javascript' src='js/app.js'></script>
+                                <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'></script>
+                                <script type='text/javascript' src=''></script>
+                            </head>
+                            <body oncontextmenu='return false' class='snippet-body'>
+                            <div class="page-wrapper">
+    
+
+    <div class="custom-modal" style="background: #310a08;">
+        <div class="danger danger-animation icon-top"><i class="fa fa-times"></i></div>
+        <div class="danger border-bottom"></div>
+        <div class="content">
+            <p class="type">Oops</p>
+            <p class="message-type" style="color: #dbc5c4;">Somthing went wrong <br> please try again</p>
+        </div>
+    </div>
+</div>
+<script>
+    var timer = setTimeout(function() {
+        window.location = 'index.php'
+
+     }, 3000);
+</script>
+                            <script src="js/app.js" type='text/javascript'></script>
+                            </body>
+                        </html>
